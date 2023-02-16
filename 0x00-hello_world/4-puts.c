fda194bff1e7e5dfb2e0b59e2ce7a721d80f5cff
Gmajor100 <geraldjustice100@gmail.com>
2023-02-16 11:39:15 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle"
+ * return 0.
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
