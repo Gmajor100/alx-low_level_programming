f3154481efaafdd2b687124ac32cd94b39ba4d10
Gmajor100 <geraldjustice100@gmail.com>
2023-02-22 13:06:32 -0800

    Task 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isalpha - checksfor alphabetic character
+ * _isalpha - checks for alphabetic character
  * @c: the character to be checked
- * Return: 1 for alphabetic character or 0 for anything else
+ * Return: 1 for alphabetical character or 0 for anything else
  */
 
 int _isalpha(int c)
